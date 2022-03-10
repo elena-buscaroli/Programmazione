e3b028c778684d6c3e2a7b6dfb18c5dc140c807c
Elena Buscaroli <72945212+elena-buscaroli@users.noreply.github.com>
2022-03-10 14:41:03 +0100

    Update esercizio1.c

--- Esercizi/esercizio1.c
+++ Esercizi/esercizio1.c
@@ -2,7 +2,7 @@
 
 /*
     Calcolare la media aritmetica tra N numeri x_1,...,x_N inseriti da tastiera. Quando il numero inserito è uguale a 0, vogliamo interrompere l'inserimento dei numeri e visualizzare a schermo la media dei numeri inseriti.
-     La media è calcolata come 1/N * \sum(x_i), per i=1,...,N
+    La media è calcolata come 1/N * \sum(x_i), per i=1,...,N
 */
 
 
