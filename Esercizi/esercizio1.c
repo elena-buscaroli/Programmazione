0b807e905333ef6195406cefbbd71bd1c8686410
elena-buscaroli <ele.buscaroli@gmail.com>
2022-03-10 14:33:30 +0100

    updates

--- Esercizi/esercizio1.c
+++ Esercizi/esercizio1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-// Calcolare la media aritmetica tra N numeri x_1,...,x_N inseriti da tastiera
-// La media è calcolata come media = 1/N \sum(x_i), per i=1,...,N
+/*
+    Calcolare la media aritmetica tra N numeri x_1,...,x_N inseriti da tastiera. Quando il numero inserito è uguale a 0, vogliamo interrompere l'inserimento dei numeri e visualizzare a schermo la media dei numeri inseriti.
+     La media è calcolata come 1/N * \sum(x_i), per i=1,...,N
+*/
+
 
 int main() {
 
--- Esercizi/esercizio1_sol.c
+++ Esercizi/esercizio1_sol.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-// Calcolare la media aritmetica tra N numeri x_1,...,x_N inseriti da tastiera                // La media è calcolata come media = 1/N \sum(x_i), per i=1,...,N
+/*
+    Calcolare la media aritmetica tra N numeri x_1,...,x_N inseriti da tastiera. Quando il numero inserito è uguale a 0, vogliamo interrompere l'inserimento dei numeri e visualizzare a schermo la media dei numeri inseriti.
+    La media è calcolata come 1/N * \sum(x_i), per i=1,...,N.
+*/
+
 
 int main() {
     float s = 0;  // somma provvisoria
     int c = 0;  // numero di elementi
 
-    int a = -1; 
+    int a = -1;  // variabile in cui salveremo il numero inserito da tastiera
 
+    // utilizziamo un while loop, siccome il numero di iterazioni dipende da quanti valori verranno inseriti da tastiera; iteriamo finchè non viene inserito uno 0
     while (a != 0) {
         printf("Inserisci un numero: ");
         scanf("%d", &a);
