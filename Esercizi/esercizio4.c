ac652fdec5e6099f03c90c2ffa27f95169f1d432
elena-buscaroli <ele.buscaroli@gmail.com>
2022-03-10 14:40:22 +0100

    updates

--- Esercizi/esercizio4.c
+++ Esercizi/esercizio4.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-// Dati due interi n e k, calcolare la sommatoria k^i, con i=1,...,n -> k+k^2+k^3+...+k^n
-// Per esempio: n=3 e k=3 -> risultato = k^1 + k^2 + k^3 = 3 + 9 + 27 = 39
+/*
+    Dati due interi n e k, calcolare la sommatoria k^i, con i=1,...,n -> k + k^2 + ... + k^n.
+    Esempio: assumiamo n=3 e k=3 -> ris = k^1 + k^2 + k^3 = 3 + 9 + 27 = 39.
+*/
 
 int main() {
 
