cd23a891433378d3ee64d65eafc855e18ff29c7f
Elena Buscaroli <72945212+elena-buscaroli@users.noreply.github.com>
2022-03-10 14:41:57 +0100

    Update esercizio5.c

--- Esercizi/esercizio5.c
+++ Esercizi/esercizio5.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /*
     Scrivere un programma che, preso un intero positivo da tastiera, stampi la successione di Fibonacci fino al numero inserito.
     L'n-esimo elemento della successione di Fibonacci è definito come:
@@ -8,7 +7,6 @@
 	1 se i = 1;
 	F_(n-1) + F_(n-2) se i>1;
 */
-
 
 int main() {
 
