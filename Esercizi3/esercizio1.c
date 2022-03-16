1bb470e9502b878edbbae208bcf84f5b63bee077
elena-buscaroli <ele.buscaroli@gmail.com>
2022-03-17 00:44:24 +0100

    mc

---
+++ Esercizi3/esercizio1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    // dichiaro le variabili intere v1 e v2
+
+    // dichiaro i pointers p1 e p2
+
+    // 1. stampare i valori di v1 e v2
+
+    // 2. stampare indirizzi di v1 e v2
+
+    // 3. assegnare valori diversi a v1 e v2 e stamparli
+
+    // 4. stampare i valori delle variabili puntate da p1 e p2
+
+    // 5. stampare indirizzi di p1 e p2
+
+    // 6. assegno indirizzi ai pointers p1 e p2
+
+    // 7. stampare indirizzi salvati in p1 e p2
+
+    // 8. stampare i valori delle variabili puntate da p1 e p2
+
+    // 9. incrementare v1 e stampare il suo valore
+
+    // 10. stampare l'indirizzo contenuto da p1
+
+    // 11. stampare il valore della variabile puntata da p1
+
+    // 12. assegnare a p2 l'indirizzo di v
+
+    // 13. stampare il valore della variabile puntata da p2
+
+    // 14. incrementare il valore della variabile puntata da p1, attraverso p1 stesso
+
+    // 15. stampare il valore della variabile puntata da p2
+
+    // 16. dichiarare e inizializzare il pointer a pointer p3
+
+    // 17. stampare il valore della variabile puntata da p3 (puntata a sua volta da p2), tramite p3 stesso
+
+    // 18. decrementare e stampare il valore della variabile puntata da p3 (puntata a sua volta da p2), tramite p3 stesso
+
+    return 0;
+}
---
+++ Esercizi3/esercizio3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+    Si scriva una funzione ricorsiva (possibilmente ricorsiva dal basso), che prenda come
+    input un intero positivo n e il puntatore della variabile H, che conterrà il seguente risultato:
+    H(n)=i=1n1i=1+12+...+1n
+    La funzione deve essere definita nel seguente modo: void somma_armonica(int n, double* h)
+*/
+
+void somma_armonica(int n, double *h)
+{
+}
+
+int main()
+{
+    return 0;
+}
