d51b2796017b7e9c69a979e70e7432d68f6a97f5
Elena Buscaroli <72945212+elena-buscaroli@users.noreply.github.com>
2022-03-22 09:49:05 +0000

    mc

--- Esercizi4/esercizio1.c
+++ Esercizi4/esercizio1.c
@@ -1,12 +1,13 @@
 #include <stdio.h> // scanf, printf
 
 /*
-	Considerare due array statici A e B dimensione N, ed inizializzati tramite input da tastiera.
-	1.  Per ogni indice i, sommare e moltiplicare A[i] e B[i] salvare i risultati in un terzo array C, all'indice i (usando lo stesso array di supporto, sovrascrivendo il valore per ogni operazione che deve essere eseguita);
-	2.  confrontare gli elementi A[i] e B[i] dei due array e salvare l'elemento maggiore in C[i];
-	3.  determinare la media, il valore massimo ed il valore minimo dell'array C risultante dal punto precedente. 
-	Eseguire tutte le istruzioni nel main. 
+	Considerare due array statici A e B dimensione N, ed inizializzati tramite input da tastiera. Creare tre funzioni che facciano le seguenti operazioni:
+	1.  Per ogni indice i, sommare e moltiplicare A[i] e B[i] salvare i risultati in un terzo array C, all'indice i (usando lo stesso array di supporto, sovrascrivendo il valore per ogni operazione che deve essere eseguita) -> la funzione prenda come input i tre array A,B,C e la loro dimensione;
+	2.  confrontare gli elementi A[i] e B[i] dei due array e salvare l'elemento maggiore in C[i] -> la funzione prenda come input i tre array A,B,C e la loro dimensione;
+	3.  determinare la media, il valore massimo ed il valore minimo dell'array C risultante dal punto precedente -> la funzione prenda come input l’array C, la sua dimensione e tre
+variabili min,max,mean, inizializzate nel main e passate alla funzione come puntatori.
 */
+
 
 int main()
 {
