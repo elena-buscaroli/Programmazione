54496664bbd82e50d3c544cfba702a7fc9ca9b3a
elena-buscaroli <ele.buscaroli@gmail.com>
2022-03-23 23:51:23 +0100

    mc

--- Esercizi4/esercizio2.c
+++ Esercizi4/esercizio2.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /*
-    Letti in input due array A e B, rispettivamente di N ed M elementi, scrivere una funzione che salvi in un terzo array C tutti gli elementi presenti in B, ma non in A.
+    Letti in input due array A e B, rispettivamente di N ed M elementi,
+    scrivere una funzione che salvi in un terzo array C tutti gli elementi
+    presenti in B, ma non in A, e che ritorni la lunghezza di C.
 */
 
 int main()
