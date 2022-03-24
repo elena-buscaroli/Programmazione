e35ac9304bf8b7c092a57b3e13546b505cb9890a
Elena Buscaroli <72945212+elena-buscaroli@users.noreply.github.com>
2022-03-24 11:19:11 +0000

    mc

--- Esercizi4/esercizio4.c
+++ Esercizi4/esercizio4.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /*
-    Leggere un array di 10 numeri interi e scrivere una funzione che prenda come input l'array 
-    inserito, la sua dimensione e una variabile "check", inizializzata a 0 e passata tramite puntatore, 
-    che sarà uguale a 0 se la sequenza inserita è palindroma, o 1 viceversa → se non cambia ad essere 
-    letta dalla prima cella all'ultima o viceversa.
+    Leggere un array di 10 numeri interi e scrivere una
+    funzione che prenda come input l'array inserito, la sua
+    dimensione e una variabile "check", inizializzata nel main
+    a 0 e passata alla funzione tramite puntatore, che sarà
+    uguale a 0 se la sequenza inserita è palindroma, o 1
+    viceversa → se non cambia ad essere letta dalla prima 
+    cella all'ultima o viceversa.
     Esempio: la sequenza 12321 è palindroma.
 */
 
