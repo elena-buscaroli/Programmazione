911da12e1d82c7c06b6fd6013e7b4ef14a13dbbf
elena-buscaroli <ele.buscaroli@gmail.com>
2022-03-28 00:04:53 +0200

    mm

---
+++ Esercizi4/esercizio6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+    Dato un array A di 10 elementi, scrivere una funzione che modifichi l'array
+    A stesso invertendone i valori.
+    Esempio: A={1,2,3,4,5} -> A={5,4,3,2,1}
+*/
+
+int main()
+{
+    return 0;
+}
---
+++ Esercizi4/esercizio7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+    Un numero è definito triangolare se è costituito dalla somma dei primi
+    n numeri interi positivi per un certo n.
+    Ad esempio, 10 è il quatro numero triangolare definito dalla somma 1+2+3+4 (n=4).
+    Di conseguenza, per un valore di n, possiamo trovare il numero triangolare n:
+    - T[n] = 1 se n=1
+    - T[n] = T[n-1] + n se n>1.
+
+    Scrivere una funzione che prenda come input un array A e un intero n, e inserisca
+    in A[i] il valore dell'i-esimo numero triangolare.
+    Scrivere la funzione in modo ricorsivo.
+*/
+
+int main()
+{
+    return 0;
+}
